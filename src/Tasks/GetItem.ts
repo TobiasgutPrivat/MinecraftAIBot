b4c64546cbc168a38db486af3313e02c4eaa2810
TobiasgutPrivat <79203201+TobiasgutPrivat@users.noreply.github.com>
2026-01-07 18:16:17 +0100

    start gather item

--- src/Tasks/GetItem.ts
+++ src/Tasks/GetItem.ts
@@ -5,3 +5,12 @@ function GetItem(bot: Bot, item: string) {
     checkAbort(bot);
     //get item
 }
+
+function gatherItem(bot: Bot, item: id) { // get id from bot.bot.registry.itemsByName[item].id
+    checkAbort(bot);
+    //gather item
+    bot.bot.registry.
+
+    const blocks = Object.values(bot.bot.registry.blocks)
+    .filter(block => block.drops?.includes(item));
+}
