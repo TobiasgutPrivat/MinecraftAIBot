ebf87fe0f18a945f5cb8726847cc362c8ce6a929
TobiasgutPrivat <tobias.gut.03@gmail.com>
2026-01-07 13:35:58 +0100

    working bot

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,7 +1,24 @@
+import mineflayer from "mineflayer";
+import { pathfinder } from "mineflayer-pathfinder";
+
 class Bot {
+    bot: mineflayer.Bot;
     username: string;
     
     constructor(username: string) {
         this.username = username;
+        this.bot = mineflayer.createBot({username: username});
+
+        this.bot.loadPlugin(pathfinder)
+    
+        this.bot.on('spawn', () => {
+            this.bot.chat("Hello world!");
+        })
+        this.bot.on('chat', (username, message) => {
+            if (username === this.username) return
+            this.bot.chat("Hello response!");
+        })
     }
 }
+
+export default Bot
